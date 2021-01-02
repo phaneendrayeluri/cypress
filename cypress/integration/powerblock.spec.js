933b1fb290db36578eddcf41baf751ca31714fd4
Sai Phaneendra Yeluri <sai.yeluri@mheducation.com>
2021-01-01 20:44:14 -0500

    Fix for a flaky website

--- cypress/integration/powerblock.spec.js
+++ cypress/integration/powerblock.spec.js
@@ -1,9 +1,16 @@
 describe('Powerblock Stock Checker', () => {
     it('Open Webpage', () => {
         cy.visit('https://powerblock.com/product/elite-series/')
-        cy.contains('Add to cart', { matchCase: false }).click()
-        cy.get('.cart-count').first().then(($span) => {
-            expect($span.text()).to.equal('0')
+        cy.get('body').then($body => {
+            if ($body.find('p.insufficient-stock').length > 0) {
+                cy.log("No Stock, See ya later.")
+            } else {
+                cy.contains('Add to cart', { matchCase: false }).click()
+                cy.get('.cart-count').first().then(($span) => {
+                    expect($span.text()).to.equal('0')
+                })
+            }
         })
+
     })
 })
